972d8b7d5f0e8029140165772d295f171c3f7b53
Erasmus Antwi <refugedesigns17@gmail.com>
2022-03-07 06:39:23 +0000

    updated redux state to use reselect for memoization

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
+import { connect } from 'react-redux'
+
 import "./cart-dropdown.styles.scss"
 
 import CustomButton from "../ui-components/custom-button/CustomButton"
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import CartItem from '../cart-item/CartItem';
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {
+          cartItems?.map(({id, ...otherProps}) => (
+            <CartItem key={id} {...otherProps} />
+          ))
+        }
+      </div>
       <CustomButton> GO TO CHECKOUT </CustomButton>
     </div>
   );
 }
 
-export default CartDropdown
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown)
